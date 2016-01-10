45dcc19e90d04d76a880f6eff4ad0b0034385d3c
chenzejian <chenzejian@youmi.net>
2016-01-10 14:26:36 +0800

    上传

--- blog/settings.py
+++ blog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap_toolkit',
+    'home',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -121,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+TEMPLATES_ROOT = os.path.join(BASE_DIR, "templates/")
